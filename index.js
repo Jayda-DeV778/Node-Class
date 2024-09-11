6bed24139d17d7a8858a2a83ef789d964d626235
Jomiloju <jaydboy824@gmail.com>
2024-09-11 21:35:28 +0100

    Assignment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
 
 let userarray = []
+let todoarray = []
 let errormessage = ""
 
 
@@ -18,6 +19,18 @@ app.get("/login", (request, response) => {
 
 app.get("/dashboard", (request, response) => {
     response.render('dashboard')
+})
+
+app.get("/todo", (request, response) => {
+    response.render("todo", {todoarray})
+})
+
+app.get("/edit/:index",(request, response)=>{
+    console.log(request.params);
+    const{ index } = request.params
+    console.log(index);
+    let editTodo = todoarray[index]
+    response.render("edit", {editTodo , index})
 })
 
 app.post("/register", (request, response) => {
@@ -36,16 +49,42 @@ app.post("/register", (request, response) => {
 
 })
 
-app.post("/login", (request, response)=> {
-   let confirmUser = userarray.find((user)=> user.email === request.body.email && user.password === request.body.password)
+app.post("/login", (request, response) => {
+    let confirmUser = userarray.find((user) => user.email === request.body.email && user.password === request.body.password)
     if (confirmUser) {
         response.redirect("/dashboard")
         errormessage = ''
     } else {
-        errormessage = "Invalid email or password";  
-        response.redirect('/login'); 
+        errormessage = "Invalid email or password";
+        response.redirect('/login');
     }
 })
+
+app.post("/addtodo", (request, response) => {
+    console.log(request.body);
+    todoarray.push(request.body)
+    console.log(todoarray);
+    response.redirect("/todo")
+})
+
+app.post("/delete",(request, response)=>{
+    console.log(request.params);
+    let index = reques.params.index
+    console.log(index);
+    todoarray.splice(index,1)
+    console.log(request.params);
+    response.redirect("/todo")
+})
+
+app.post("/editted/:index",(request , response)=>{
+    console.log(request.body);
+    const { index } = request.params
+    let change = request.body
+    todoarray[index] = change
+    response.redirect("/todo")
+    
+})
+
 
 
 const port = 8005
